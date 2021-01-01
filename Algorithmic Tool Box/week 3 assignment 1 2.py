412807301066a27fea29e4f2592931ee2ddf371b
toushi100 <toushi2009@gmail.com>
2021-01-01 14:52:22 +0200

    commit

--- Algorithmic Tool Box/week 3 assignment 1 2.py
+++ Algorithmic Tool Box/week 3 assignment 1 2.py
@@ -9,8 +9,6 @@ def fractionalknapsack(capacity, weights, values):
     print(weights)
     print(values)
 
-
-
     for i in range(n):
         if curr_weight + weights[i] <= capacity:
             curr_weight += weights[i]
@@ -23,27 +21,28 @@ def fractionalknapsack(capacity, weights, values):
 
     return curr_value
 
-
-
     if capacity == 0:
         return 0
     for i in range(n):
         max_index = select_max_index(values, weights)
         if max_index >= 0:
             available_weights = min(capacity, weights[max_index])
-            value = value + available_weights * values[max_index]/weights[max_index]
+            value = value + available_weights * values[max_index] / weights[max_index]
             weights[max_index] = weights[max_index] - available_weights
             capacity = capacity - available_weights
 
     return value
+
+
 def select_max_index(values, weights):
     index = -1
     max = 0
     for i in range(n):
         if weights[i] > 0 and (values[i] / weights[i]) > max:
-            max = values[i]/weights[i]
+            max = values[i] / weights[i]
             index = i
     return index
+
 
 if __name__ == "__main__":
     data = list(map(int, sys.stdin.read().split()))
@@ -52,4 +51,3 @@ if __name__ == "__main__":
     weights = data[3:(2 * n + 2):2]
     opt_value = fractionalknapsack(capacity, weights, values)
     print("{:.10f}".format(opt_value))
-
