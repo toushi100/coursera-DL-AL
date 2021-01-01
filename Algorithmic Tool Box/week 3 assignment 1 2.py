60fc6a4a0577b34e8f6501ca303c2ee50013970d
toushi100 <toushi2009@gmail.com>
2021-01-01 14:49:38 +0200

    fixed conflicts

--- Algorithmic Tool Box/week 3 assignment 1 2.py
+++ Algorithmic Tool Box/week 3 assignment 1 2.py
@@ -2,20 +2,6 @@
 import sys
 
 
-def get_optimal_value(capacity, weights, values):
-    value = 0.
-    arr = [0 for _ in range(n)]
-    for i in range(n):
-        if capacity == 0:
-            return value
-        arr[i] = values[i] / weights[i]
-        arr.sort(reverse=True)
-
-    print(arr)
-
-    return value
-
-
 def fractionalknapsack(capacity, weights, values):
     curr_weight = 0
     curr_value = 0
@@ -38,6 +24,27 @@ def fractionalknapsack(capacity, weights, values):
     return curr_value
 
 
+
+    if capacity == 0:
+        return 0
+    for i in range(n):
+        max_index = select_max_index(values, weights)
+        if max_index >= 0:
+            available_weights = min(capacity, weights[max_index])
+            value = value + available_weights * values[max_index]/weights[max_index]
+            weights[max_index] = weights[max_index] - available_weights
+            capacity = capacity - available_weights
+
+    return value
+def select_max_index(values, weights):
+    index = -1
+    max = 0
+    for i in range(n):
+        if weights[i] > 0 and (values[i] / weights[i]) > max:
+            max = values[i]/weights[i]
+            index = i
+    return index
+
 if __name__ == "__main__":
     data = list(map(int, sys.stdin.read().split()))
     n, capacity = data[0:2]
@@ -45,3 +52,4 @@ if __name__ == "__main__":
     weights = data[3:(2 * n + 2):2]
     opt_value = fractionalknapsack(capacity, weights, values)
     print("{:.10f}".format(opt_value))
+
